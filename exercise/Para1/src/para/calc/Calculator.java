2c861292ff0c19126860d5ba8d956b9a2c8210e9
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-16 22:32:23 +0900

    exercise1

---
+++ exercise/Para1/src/para/calc/Calculator.java
@@ -0,0 +1,78 @@
+package para.calc;
+
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.geometry.Pos;
+import javafx.scene.shape.*;
+import javafx.scene.paint.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+/**
+ * JavaFX 電卓アプリケーションのメインクラス
+ */
+public class Calculator extends Application{
+  Label input;
+  Label output;
+  StringBuilder buff;
+  Executor ex;
+  public Calculator(){
+    input = new Label();
+    output = new Label();
+    buff = new StringBuilder();
+    ex = new Executor1();
+  }
+  String[] buttonname = {"9","8","7","+",
+                         "6","5","4","-",
+                         "3","2","1","*",
+                         "0",".",",","/"};
+
+  public void start(Stage stage){
+    VBox root = new VBox();
+    root.setAlignment(Pos.TOP_CENTER);
+    GridPane grid = new GridPane();
+    Scene scene = new Scene(root, 200,300);
+    Button[] buttons = new Button[16];
+    Button buttoncal = new Button("=");
+    buttoncal.setPrefHeight(56);
+    Button buttondel = new Button("<");
+    StackPane stack = new StackPane();
+    stack.getChildren().add(new Rectangle(140,30,Color.WHITE));
+    stack.getChildren().add(input);
+    root.getChildren().addAll(stack, output);
+    for(int i=0;i<16;i++){
+      buttons[i] = new Button(buttonname[i]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    stage.setWidth(200);
+    stage.setHeight(200);
+    stage.setScene(scene);
+    stage.setTitle("JavaFX Calc");
+    stage.show();
+  }
+}
---
+++ exercise/Para1/src/para/paint/Paint.java
@@ -0,0 +1,98 @@
+package para.paint;
+
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.geometry.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+import javafx.scene.canvas.*;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
+import javafx.scene.paint.Color;
+import javafx.beans.value.ObservableValue;
+
+/**
+ * JavaFX お絵描きアプリケーションのメインクラス
+ */
+public class Paint extends Application{
+  Canvas canvas;
+  GraphicsContext gc;
+  /** 直前のポインタのx座標 */
+  double oldx;
+  /** 直前のポインタのy座標 */
+  double oldy;
+  /** 描画領域の大きさ */
+  final int SIZE=600;
+  Button clear;
+
+  /**
+   * お絵描きプログラムの準備をして、ウィンドウを開きます
+   */
+  public void start(Stage stage){
+    Group group = new Group();
+    canvas = new Canvas(SIZE,SIZE);
+    gc = canvas.getGraphicsContext2D();
+    drawShapes(gc);
+    canvas.setOnMousePressed(ev->{
+        oldx = ev.getX();
+        oldy = ev.getY();
+      });
+
+    canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
+                           new EventHandler<MouseEvent>(){
+                             public void handle(MouseEvent ev){
+                               gc.strokeLine(oldx, oldy, ev.getX(), ev.getY());
+                               oldx = ev.getX();
+                               oldy = ev.getY();
+                             }
+                           });
+    clear = new Button("clear");
+    clear.setOnAction(e->{
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0,0,SIZE,SIZE);});
+
+    BorderPane bp = new BorderPane();
+    VBox vb = new VBox();
+    Slider sliderr = new Slider(0, 1, 0);
+    Slider sliderg = new Slider(0, 1, 0);
+    Slider sliderb = new Slider(0, 1, 1);
+    sliderr.valueProperty().addListener((ObservableValue<? extends Number> ov,
+                                         Number oldv, Number nv)->{
+      });
+    vb.setAlignment(Pos.CENTER);
+    vb.getChildren().add(sliderr);
+    vb.getChildren().add(sliderg);
+    vb.getChildren().add(sliderb);
+    vb.getChildren().add(clear);
+    bp.setTop(vb);
+    bp.setCenter(canvas);
+    Scene scene = new Scene(bp);
+    stage.setScene(scene);
+    stage.setTitle("JavaFX Draw");
+    stage.show();
+  }
+
+  /**
+   * 初期化メソッド、startメソッドの呼び出され方とは異なる呼び出され方をする。必要ならば定義する
+   */
+  public void init(){
+  }
+
+  /**
+   * 図形を描きます。
+   * 図形描画の実装サンプルです
+   */
+  private void drawShapes(GraphicsContext gc) {
+    gc.setFill(Color.WHITE);
+    gc.fillRect(0,0,SIZE,SIZE);
+    gc.setFill(Color.GREEN);
+    gc.setStroke(Color.BLUE);
+    gc.setLineWidth(4);
+    gc.strokeLine(40, 10, 10, 40);
+    gc.fillOval(60, 10, 30, 30);
+    gc.strokeOval(110, 10, 30, 30);
+    gc.fillRoundRect(160, 10, 30, 30, 10, 10);
+  }
+}
