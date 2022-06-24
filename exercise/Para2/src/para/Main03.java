ddef8405a2fd87a10388f2e0152a929084231266
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-24 10:43:44 +0900

    format

--- exercise/Para2/src/para/Main03.java
+++ exercise/Para2/src/para/Main03.java
@@ -1,4 +1,5 @@
 package para;
+
 import para.graphic.shape.Attribute;
 import para.graphic.shape.Circle;
 import para.graphic.shape.Image;
@@ -10,39 +11,42 @@ import java.io.*;
 import java.awt.image.*;
 
 /** 画像ファイルを画像図形に利用して表示するデモ */
-public class Main03{
+public class Main03 {
   /** 画像ファイル名 */
   private final static String IMAGEFILENAME = "sweet.png";
-  /** メインメソッド
+
+  /**
+   * メインメソッド
+   * 
    * @param args このプログラムでは値は使用されません
    */
-  public static void main(String[] args){
+  public static void main(String[] args) {
     ShapeManager sm = new ShapeManager();
     Target target;
     target = new JavaFXTarget("DisplayShapes", 300, 300);
-//    target = new TextTarget(System.out);
+    // target = new TextTarget(System.out);
     target.init();
-    File imagefile=null;
+    File imagefile = null;
     BufferedImage bimage = null;
     try {
       imagefile = new File(IMAGEFILENAME);
       bimage = ImageIO.read(imagefile);
-    }catch(IOException e){
+    } catch (IOException e) {
       System.err.println(e);
     }
 
-    while(true){
+    while (true) {
       target.clear();
-      for(int i=0;i<256;i=i+50){
-        for(int ii=0;ii<256;ii=ii+50){
-          Shape s = new Image(i*256+ii,ii,i,bimage,
-                              new Attribute(ii, i, 255-i, true));
+      for (int i = 0; i < 256; i = i + 50) {
+        for (int ii = 0; ii < 256; ii = ii + 50) {
+          Shape s = new Image(i * 256 + ii, ii, i, bimage,
+              new Attribute(ii, i, 255 - i, true));
           sm.add(s);
           target.draw(sm);
           target.flush();
-          try{
+          try {
             Thread.sleep(200);
-          }catch(InterruptedException e){
+          } catch (InterruptedException e) {
           }
         }
       }
