16ce9cab098d2a0ea76b603584489d43373a538c
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-24 10:54:46 +0900

    format

--- exercise/Para2/src/para/calc/Calculator.java
+++ exercise/Para2/src/para/calc/Calculator.java
@@ -13,48 +13,55 @@ import javafx.scene.control.*;
  * JavaFX 電卓アプリケーションのメインクラス
  */
 public class Calculator extends Application {
-  Label input;
-  Label output;
+  Label inputLabel;
+  Label outputLabel;
   StringBuilder buff;
-  Executor ex;
+  Executor executor;
 
   public Calculator() {
-    input = new Label();
-    output = new Label();
+    inputLabel = new Label();
+    outputLabel = new Label();
     buff = new StringBuilder();
-    ex = new Executor1();
+    executor = new Executor1();
   }
 
-  String[] buttonname = { "9", "8", "7", "+",
+  String[] buttonName = { "9", "8", "7", "+",
       "6", "5", "4", "-",
       "3", "2", "1", "*",
       "0", ".", ",", "/" };
 
   public void start(Stage stage) {
+
     VBox root = new VBox();
     root.setAlignment(Pos.TOP_CENTER);
+
     GridPane grid = new GridPane();
     Scene scene = new Scene(root, 200, 200);
     Button[] buttons = new Button[16];
-    Button buttoncal = new Button("=");
-    double tmph = buttoncal.getHeight();
-    buttoncal.setPrefHeight(56);
-    Button buttondel = new Button("<");
-    buttondel.setPrefHeight(56);
+
+    Button buttonCalculation = new Button("=");
+    double tmph = buttonCalculation.getHeight();
+    buttonCalculation.setPrefHeight(56);
+
+    Button buttonDelete = new Button("<");
+    buttonDelete.setPrefHeight(56);
+
     StackPane stack = new StackPane();
     stack.getChildren().add(new Rectangle(140, 30, Color.WHITE));
-    stack.getChildren().add(input);
-    root.getChildren().addAll(stack, output);
+    stack.getChildren().add(inputLabel);
+    root.getChildren().addAll(stack, outputLabel);
     root.getChildren().add(grid);
     grid.setAlignment(Pos.CENTER);
+
     for (int i = 0; i < 16; i++) {
-      buttons[i] = new Button(buttonname[i]);
+      buttons[i] = new Button(buttonName[i]);
       buttons[i].setPrefHeight(26);
       buttons[i].setPrefWidth(26);
       grid.add(buttons[i], i % 4, i / 4);
     }
-    grid.add(buttondel, 4, 0, 1, 2);
-    grid.add(buttoncal, 4, 2, 1, 2);
+
+    grid.add(buttonDelete, 4, 0, 1, 2);
+    grid.add(buttonCalculation, 4, 2, 1, 2);
 
     stage.setScene(scene);
     stage.setTitle("JavaFX Calc");
