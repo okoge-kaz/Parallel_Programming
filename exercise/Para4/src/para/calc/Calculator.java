128c9ba56125f97dd0f86be8f5e5effaccd39953
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-08 11:07:18 +0900

    format

--- exercise/Para4/src/para/calc/Calculator.java
+++ exercise/Para4/src/para/calc/Calculator.java
@@ -12,7 +12,7 @@ import javafx.scene.control.*;
 /**
  * JavaFX 電卓アプリケーションのメインクラス
  */
-public class Calculator extends Application{
+public class Calculator extends Application {
   /** 入力数式の表示部 */
   final Label input;
   /** 計算結果の表示部 */
@@ -21,26 +21,29 @@ public class Calculator extends Application{
   final StringBuilder buff;
   /** 数式解釈器 */
   final Executor ex;
-  /** 電卓の状態を保持するデータ領域、逆ポーランド記法解釈器の準備といった初期化行う.
+
+  /**
+   * 電卓の状態を保持するデータ領域、逆ポーランド記法解釈器の準備といった初期化行う.
    */
-  public Calculator(){
+  public Calculator() {
     input = new Label();
     output = new Label();
     buff = new StringBuilder();
     ex = new Executor1();
-    //ex = new Executor2(output);
+    // ex = new Executor2(output);
   }
-  /** ボタンのラベル */
-  final String[] buttonname = {"9","8","7","+",
-                               "6","5","4","-",
-                               "3","2","1","*",
-                               "0",".",",","/"};
 
-  public void start(Stage stage){
+  /** ボタンのラベル */
+  final String[] buttonname = { "9", "8", "7", "+",
+      "6", "5", "4", "-",
+      "3", "2", "1", "*",
+      "0", ".", ",", "/" };
+
+  public void start(Stage stage) {
     VBox root = new VBox();
     root.setAlignment(Pos.TOP_CENTER);
     GridPane grid = new GridPane();
-    Scene scene = new Scene(root, 200,300);
+    Scene scene = new Scene(root, 200, 300);
     Button[] buttons = new Button[16];
     Button buttoncal = new Button("=");
     double tmph = buttoncal.getHeight();
@@ -48,44 +51,44 @@ public class Calculator extends Application{
     Button buttondel = new Button("<");
     buttondel.setPrefHeight(56);
     StackPane stack = new StackPane();
-    stack.getChildren().add(new Rectangle(140,30,Color.WHITE));
+    stack.getChildren().add(new Rectangle(140, 30, Color.WHITE));
     stack.getChildren().add(input);
-    for(int i=0;i<16;i++){
+    for (int i = 0; i < 16; i++) {
       buttons[i] = new Button(buttonname[i]);
       buttons[i].setPrefHeight(26);
-      if(i%4 == 3){
-        buttons[i].setOnAction(ev->{
-            buff.append(((Button)ev.getSource()).getText()+",");
-            input.setText(buff.toString());
-          });
-      }else{
-        buttons[i].setOnAction(ev->{
-            buff.append(((Button)ev.getSource()).getText());
-            input.setText(buff.toString());
-          });
+      if (i % 4 == 3) {
+        buttons[i].setOnAction(ev -> {
+          buff.append(((Button) ev.getSource()).getText() + ",");
+          input.setText(buff.toString());
+        });
+      } else {
+        buttons[i].setOnAction(ev -> {
+          buff.append(((Button) ev.getSource()).getText());
+          input.setText(buff.toString());
+        });
       }
     }
-    buttoncal.setOnAction(ev->{
-        System.out.println("[["+buff.toString()+"]]");
-        String mid;
-        mid = ex.operation(buff.toString());
-        output.setText(mid);
-        buff.delete(0,buff.length());
-      });
-    buttondel.setOnAction(ev->{
-        if(buff.length()!=0){
-          buff.deleteCharAt(buff.length()-1);
-        }
-        input.setText(buff.toString());
-      });
+    buttoncal.setOnAction(ev -> {
+      System.out.println("[[" + buff.toString() + "]]");
+      String mid;
+      mid = ex.operation(buff.toString());
+      output.setText(mid);
+      buff.delete(0, buff.length());
+    });
+    buttondel.setOnAction(ev -> {
+      if (buff.length() != 0) {
+        buff.deleteCharAt(buff.length() - 1);
+      }
+      input.setText(buff.toString());
+    });
     root.getChildren().addAll(stack, output);
     root.getChildren().add(grid);
     grid.setAlignment(Pos.CENTER);
-    for(int i=0;i<16;i++){
-      grid.add(buttons[i],i%4,i/4);
+    for (int i = 0; i < 16; i++) {
+      grid.add(buttons[i], i % 4, i / 4);
     }
-    grid.add(buttondel,4,0,1,2);
-    grid.add(buttoncal,4,2,1,2);
+    grid.add(buttondel, 4, 0, 1, 2);
+    grid.add(buttoncal, 4, 2, 1, 2);
     stage.setWidth(200);
     stage.setHeight(200);
     stage.setScene(scene);
