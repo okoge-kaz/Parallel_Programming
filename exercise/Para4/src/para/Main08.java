10cce251ffb9108e139bc8cb94443d70b1b53334
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-12 23:21:13 +0900

    format

--- exercise/Para4/src/para/Main08.java
+++ exercise/Para4/src/para/Main08.java
@@ -1,38 +1,45 @@
 package para;
 
-import java.io.*;
-import java.net.*;
-import para.graphic.shape.*;
-import para.graphic.target.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
 
-/** カメラを起動し、サーバに送るデモ
+import para.graphic.shape.Camera;
+import para.graphic.shape.ShapeManager;
+import para.graphic.target.Target;
+import para.graphic.target.TextTarget;
+
+/**
+ * カメラを起動し、サーバに送るデモ
  */
-public class Main08{
-  /** メインメソッド
+public class Main08 {
+  /**
+   * メインメソッド
+   * 
    * @param args args[0]は相手先のホスト
    */
-  public static void main(String[] args){
+  public static void main(String[] args) {
     final int PORTNO = 30000;
     ShapeManager sm = new ShapeManager();
-    sm.put(new Camera(0,0,0));
-    try(Socket s = new Socket(args[0],PORTNO)){
+    sm.put(new Camera(0, 0, 0));
+    try (Socket s = new Socket(args[0], PORTNO)) {
       PrintStream ps = new PrintStream(s.getOutputStream());
       Target target = new TextTarget(ps);
-      //Target target = new TextTarget(System.out);
+      // Target target = new TextTarget(System.out);
       target.init();
       target.clear();
-      while(true){
+      while (true) {
         target.draw(sm);
         target.flush();
-        try{
+        try {
           Thread.sleep(100);
-        }catch(InterruptedException ex){
+        } catch (InterruptedException ex) {
         }
-        if(ps.checkError()){
+        if (ps.checkError()) {
           break;
         }
       }
-    }catch(IOException ex){
+    } catch (IOException ex) {
       System.err.println(ex);
     }
   }
