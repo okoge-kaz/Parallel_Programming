3cba16f4349cc15c8dc37a7596e5cd900fcdd67f
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-12 10:06:30 +0900

    format and change variable names

--- exercise/Para4/src/para/calc/Calculator.java
+++ exercise/Para4/src/para/calc/Calculator.java
@@ -17,23 +17,23 @@ import javafx.stage.Stage;
  */
 public class Calculator extends Application {
   /** 入力数式の表示部 */
-  final Label input;
+  final Label inputLabel;
   /** 計算結果の表示部 */
-  final Label output;
+  final Label outputLabel;
   /** 入力数式の文字列作成用バッファ */
   final StringBuilder buff;
   /** 数式解釈器 */
-  final Executor ex;
+  final Executor interpreter;
 
   /**
    * 電卓の状態を保持するデータ領域、逆ポーランド記法解釈器の準備といった初期化行う.
    */
   public Calculator() {
-    input = new Label();
-    output = new Label();
+    inputLabel = new Label();
+    outputLabel = new Label();
     buff = new StringBuilder();
-    ex = new Executor1();
-    // ex = new Executor2(output);
+    // ex = new Executor1();
+    interpreter = new Executor2(outputLabel);
   }
 
   /** ボタンのラベル */
@@ -55,36 +55,36 @@ public class Calculator extends Application {
     buttondel.setPrefHeight(56);
     StackPane stack = new StackPane();
     stack.getChildren().add(new Rectangle(140, 30, Color.WHITE));
-    stack.getChildren().add(input);
+    stack.getChildren().add(inputLabel);
     for (int i = 0; i < 16; i++) {
       buttons[i] = new Button(buttonname[i]);
       buttons[i].setPrefHeight(26);
       if (i % 4 == 3) {
         buttons[i].setOnAction(ev -> {
           buff.append(((Button) ev.getSource()).getText() + ",");
-          input.setText(buff.toString());
+          inputLabel.setText(buff.toString());
         });
       } else {
         buttons[i].setOnAction(ev -> {
           buff.append(((Button) ev.getSource()).getText());
-          input.setText(buff.toString());
+          inputLabel.setText(buff.toString());
         });
       }
     }
     buttoncal.setOnAction(ev -> {
       System.out.println("[[" + buff.toString() + "]]");
       String mid;
-      mid = ex.operation(buff.toString());
-      output.setText(mid);
+      mid = interpreter.operation(buff.toString());
+      outputLabel.setText(mid);
       buff.delete(0, buff.length());
     });
     buttondel.setOnAction(ev -> {
       if (buff.length() != 0) {
         buff.deleteCharAt(buff.length() - 1);
       }
-      input.setText(buff.toString());
+      inputLabel.setText(buff.toString());
     });
-    root.getChildren().addAll(stack, output);
+    root.getChildren().addAll(stack, outputLabel);
     root.getChildren().add(grid);
     grid.setAlignment(Pos.CENTER);
     for (int i = 0; i < 16; i++) {
