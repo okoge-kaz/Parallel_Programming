0e0b1ab1ab8d500ddc886b5ebe951cf91bcd6a98
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-15 11:44:20 +0900

    refactor

--- exercise/Para5/src/para/Main09.java
+++ exercise/Para5/src/para/Main09.java
@@ -1,19 +1,10 @@
 package para;
 
-import javafx.application.Application;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.control.Slider;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.Group;
-
-import javafx.scene.input.MouseEvent;
-import javafx.event.EventHandler;
-import javafx.beans.value.ObservableValue;
-
-import para.graphic.target.*;
-import para.graphic.shape.*;
-import para.graphic.parser.*;
+import para.graphic.shape.Camera;
+import para.graphic.shape.OrderedShapeManager;
+import para.graphic.shape.ShapeManager;
+import para.graphic.target.JavaFXTarget;
+import para.graphic.target.Target;
 
 public class Main09 {
   public static void main(String[] args) {
@@ -21,8 +12,13 @@ public class Main09 {
     // Target target = new JavaFXTarget("color filter", 320,240);
     Target target = new TargetColorFilter(new JavaFXTarget("color filter",
         320, 240), 4);
+
     target.init();
     sm.put(new Camera(0, 0, 0));
+
+    /*
+     * 80 ms ごとの再レンダリング
+     */
     while (true) {
       target.clear();
       target.draw(sm);
