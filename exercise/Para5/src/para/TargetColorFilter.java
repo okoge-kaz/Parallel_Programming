fffa2db64178fc99223ab4588f1778b187c4ea52
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-15 11:44:48 +0900

    change variable names

--- exercise/Para5/src/para/TargetColorFilter.java
+++ exercise/Para5/src/para/TargetColorFilter.java
@@ -13,9 +13,9 @@ import para.graphic.target.TargetFilter;
  * カメラ画像の図形を出力する際、減色処理を行うフィルタを実装したTargetの装飾クラス。
  */
 public class TargetColorFilter extends TargetFilter {
-  protected int[] inimage = new int[Camera.WIDTH * Camera.HEIGHT];
-  protected byte[] outimage = new byte[Camera.WIDTH * Camera.HEIGHT * 4];
-  protected ByteBuffer outbuffer = ByteBuffer.wrap(outimage);
+  protected int[] inImage = new int[Camera.WIDTH * Camera.HEIGHT];
+  protected byte[] outImage = new byte[Camera.WIDTH * Camera.HEIGHT * 4];
+  protected ByteBuffer outbuffer = ByteBuffer.wrap(outImage);
   protected int sample[];
 
   /**
@@ -33,7 +33,7 @@ public class TargetColorFilter extends TargetFilter {
     input.rewind();
     for (int i = 0; i < input.limit() / 3; i++) {
       input.get(tmp, 0, 3);
-      inimage[i] = ((tmp[2] & 0xff) << 16) | ((tmp[1] & 0xff) << 8) | (tmp[0] & 0xff);
+      inImage[i] = ((tmp[2] & 0xff) << 16) | ((tmp[1] & 0xff) << 8) | (tmp[0] & 0xff);
     }
     Map<Integer, Integer> classified = IntStream.range(0, Camera.WIDTH * Camera.HEIGHT)
         .boxed()
@@ -42,7 +42,7 @@ public class TargetColorFilter extends TargetFilter {
               int min = 600000;
               int label = 0;
               for (int i = 0; i < sample.length; i++) {
-                int dp = distancePow(inimage[n], sample[i]);
+                int dp = distancePow(inImage[n], sample[i]);
                 if (dp < min) {
                   label = i;
                   min = dp;
@@ -51,17 +51,17 @@ public class TargetColorFilter extends TargetFilter {
               return label;
             }));
     classified.forEach((k, v) -> {
-      outimage[k * 4 + 0] = (byte) 0xff;
-      outimage[k * 4 + 1] = (byte) ((sample[v] >> 16) & 0xff);
-      outimage[k * 4 + 2] = (byte) ((sample[v] >> 8) & 0xff);
-      outimage[k * 4 + 3] = (byte) (sample[v] & 0xff);
+      outImage[k * 4 + 0] = (byte) 0xff;
+      outImage[k * 4 + 1] = (byte) ((sample[v] >> 16) & 0xff);
+      outImage[k * 4 + 2] = (byte) ((sample[v] >> 8) & 0xff);
+      outImage[k * 4 + 3] = (byte) (sample[v] & 0xff);
     });
     /*
      * do no filtering, just through output
-     * classified.forEach((k,v)->{outimage[k*4+0]=(byte)0xff;
-     * outimage[k*4+1]=(byte)((inimage[k]>>16)&0xff);
-     * outimage[k*4+2]=(byte)((inimage[k]>>8)&0xff);
-     * outimage[k*4+3]=(byte)(inimage[k]&0xff);});
+     * classified.forEach((k,v)->{outImage[k*4+0]=(byte)0xff;
+     * outImage[k*4+1]=(byte)((inImage[k]>>16)&0xff);
+     * outImage[k*4+2]=(byte)((inImage[k]>>8)&0xff);
+     * outImage[k*4+3]=(byte)(inImage[k]&0xff);});
      */
     input.rewind();
     outbuffer.rewind();
@@ -72,6 +72,7 @@ public class TargetColorFilter extends TargetFilter {
     int distanceRed = ((a & 0xff0000) >> 16) - ((b & 0xff0000) >> 16);
     int distanceGreen = ((a & 0x00ff00) >> 8) - ((b & 0x00ff00) >> 8);
     int distanceBlue = ((a & 0x0000ff)) - ((b & 0x0000ff));
+
     return distanceRed * distanceRed + distanceGreen * distanceGreen + distanceBlue * distanceBlue;
   }
 
