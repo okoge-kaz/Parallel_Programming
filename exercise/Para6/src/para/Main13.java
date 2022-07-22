de47adf8ddfa142b170c61d2cc781ad71be80fb0
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-22 10:44:03 +0900

    format Main files

--- exercise/Para6/src/para/Main13.java
+++ exercise/Para6/src/para/Main13.java
@@ -1,40 +1,46 @@
 package para;
-import para.graphic.shape.*;
-import para.graphic.target.*;
-import para.graphic.opencl.*;
 
-/** カメラ画像をOpenCLで加工して表示する例
+import para.graphic.shape.Camera;
+import para.graphic.shape.OrderedShapeManager;
+import para.graphic.shape.ShapeManager;
+import para.graphic.target.JavaFXTarget;
+import para.graphic.target.Target;
+
+/**
+ * カメラ画像をOpenCLで加工して表示する例
  * カラー画像をモザイク化する
  */
-public class Main13{
-  public Main13(){
+public class Main13 {
+  public Main13() {
   }
 
-  public void start(){
+  public void start() {
     Target target = new TargetMosaicFilter(
-                      new JavaFXTarget("MosaicFilter",360,280),
-                      "mosaic.cl", "Mosaic");
+        new JavaFXTarget("MosaicFilter", 360, 280),
+        "mosaic.cl", "Mosaic");
     ShapeManager sm = new OrderedShapeManager();
-    sm.add(new Camera(20,20,20));
+    sm.add(new Camera(20, 20, 20));
     target.init();
     target.clear();
     target.flush();
-    while(true){
+    while (true) {
       target.clear();
       target.draw(sm);
       target.flush();
-      try{
+      try {
         Thread.sleep(10);
-      }catch(InterruptedException e){
+      } catch (InterruptedException e) {
       }
     }
   }
-  
-  /** メインメソッド
+
+  /**
+   * メインメソッド
+   * 
    * @param args このプログラムでは値は使用されません
    */
-  public static void main(String[] args){
-    Main13 main= new Main13();
+  public static void main(String[] args) {
+    Main13 main = new Main13();
     main.start();
   }
 }
