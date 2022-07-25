2d84855f761c253fd0ab01ae0f0f4a7181063266
kazuki <kaz.tokyo.tech20@gmail.com>
2022-07-26 01:48:21 +0900

    done 課題1-1

--- exercise/Para6/resource/para/opencl/max3.cl
+++ exercise/Para6/resource/para/opencl/max3.cl
@@ -1,10 +1,10 @@
 #pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
 
-__kernel void Max3(__global float *a, __global float *b, __global float *c, __global float *d, int numElements)
+__kernel void Max3(__constant float *a, __constant float *b, __constant float *c, __global float *d, int numElements)
 {
     int i = get_global_id(0);
     if (i < numElements)
     {
-        d[i] = fmaxf(fmaxf(a[i], b[i]), c[i]);
+        d[i] = max(max(a[i], b[i]), c[i]);
     }
 }
--- exercise/Para6/src/para/opencl/Max3.java
+++ exercise/Para6/src/para/opencl/Max3.java
@@ -14,26 +14,102 @@ import java.util.*;
 import static java.lang.System.*;
 import static com.jogamp.opencl.CLMemory.Mem.*;
 
-/** OpenCLを使用例を知るための簡単なデモ。
- * BufferAとBufferBとBufferCのそれぞれのi番目の要素の最大値をBufferDのi番目に格納する
+/**
+ * OpenCLを使用例を知るための簡単なデモ。
+ * BufferAとBufferBとBufferCのそれぞれのi番目の要素を足してBufferDのi番目に格納する
  */
-public class Max3{
-  /** OpenCLの初期化、データ初期化、演算、結果出力、OpenCL資源の開放をする
+public class Max3 {
+  /**
+   * OpenCLの初期化、データ初期化、演算、結果出力、OpenCL資源の開放をする
    */
-  public Max3(){
-    //OpenCL演算環境の準備
+  public Max3() {
+    // OpenCL演算環境の準備
     CLSetup cl = CLSetupCreator.createCLSetup();
     cl.initContext();
 
+    FloatBuffer tmpfb;
 
+    // data a の読み込み
+    tmpfb = loadData("data/dataa.txt");
+    CLBuffer<FloatBuffer> BufferA = cl.createBuffer(tmpfb, READ_ONLY);
+    tmpfb.rewind();
 
+    // data b の読み込み
+    tmpfb = loadData("data/datab.txt");
+    CLBuffer<FloatBuffer> BufferB = cl.createBuffer(tmpfb, READ_ONLY);
+    tmpfb.rewind();
 
+    // data c の読み込み
+    tmpfb = loadData("data/datac.txt");
+    CLBuffer<FloatBuffer> BufferC = cl.createBuffer(tmpfb, READ_ONLY);
+    tmpfb.rewind();
 
+    // data d の準備
+    CLBuffer<FloatBuffer> BufferD = cl.createFloatBuffer(tmpfb.limit(),
+        WRITE_ONLY);
+    int datasize = tmpfb.limit();
+    CLCommandQueue queue = cl.createQueue();
+
+    CLProgram program = cl.createProgramFromResource(this, "max3.cl");
+    CLKernel kernel = program.createCLKernel("Max3");
+    kernel.putArgs(BufferA, BufferB, BufferC, BufferD);
+    kernel.setArg(4, datasize);
+
+    BufferD.getBuffer().rewind();
+    // デバイスへ転送、並列演算、演算結果の取得
+    queue.putWriteBuffer(BufferA, false)// BufferAのデータをカーネル側へ転送指令
+        .putWriteBuffer(BufferB, false)// BufferBのデータをカーネル側へ転送指令
+        .putWriteBuffer(BufferC, false)// BufferBのデータをカーネル側へ転送指令
+        .putBarrier() // 今までの指令がすべて完了するまで待つ
+        .put1DRangeKernel(kernel, 0, datasize, 1)// 演算指令
+        .putBarrier() // 今までの指令がすべて完了するまで待つ
+        .putReadBuffer(BufferD, true);// BufferDのデータをホスト側へ転送指令
+                                      // 転送完了まで待つ
+
+    // 演算結果の出力
+    FloatBuffer fb = BufferD.getBuffer();
+    fb.rewind();
+    for (int i = 0; i < fb.limit(); i++) {
+      System.out.print(fb.get() + " ");
+    }
+    System.out.println();
 
     cl.release();
   }
 
-  public static void main(String[] argv){
+  /**
+   * 引数で指定されたファイルから浮動小数の数値列を読み込み、その値を格納したバッファを返す。
+   * {@link CLBuffer}に組み込むBufferは
+   * 
+   * @param fname 読み込み対象となるファイルの名
+   * @return ダイレクトバッファとして領域確保した後、読み込んだ値を書き込んだ{@link FloatBuffer}
+   */
+  static public FloatBuffer loadData(String fname) {
+    ArrayList<Float> flt = new ArrayList<Float>();
+    Scanner sc = null;
+    try {
+      sc = new Scanner(new File(fname));
+    } catch (FileNotFoundException e) {
+      System.err.println(e);
+      System.exit(1);
+    }
+    while (sc.hasNextFloat()) {
+      flt.add(sc.nextFloat());
+    }
+    FloatBuffer fb = ByteBuffer.allocateDirect(flt.size() * java.lang.Float.SIZE / 8)
+        // java.lang.Float.SIZE はfloat型のbit数を意味する定数
+        .order(ByteOrder.nativeOrder())// float型を表すバイト順序にnativeを指定
+        .asFloatBuffer();
+    int i = 0;
+    while (fb.hasRemaining()) {
+      fb.put(flt.get(i));
+      i++;
+    }
+    fb.rewind();
+    return fb;
+  }
+
+  public static void main(String[] argv) {
     new Max3();
   }
 }
